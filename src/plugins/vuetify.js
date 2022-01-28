077d0f21c13cc699a0e40dcf57850b24dd0159b2
Amanda Gama <amgama17@gmail.com>
2022-01-27 23:43:05 -0300

    :sparkles: implemented vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,36 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from 'vue'
+import Vuetify, { VTextField } from 'vuetify/lib'
+import '@fortawesome/fontawesome-free/css/all.css'
+import pt from 'vuetify/lib/locale/pt'
+import en from 'vuetify/lib/locale/en'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
-});
+  icons: {
+    iconfont: 'fa',
+  },
+  theme: {
+    options: {
+      customProperties: true,
+    },
+    themes: {
+      light: {
+        primary: '#0000FF',
+        secondary: '#9D9DFF',
+        tertiary: '#C4C4C4',
+        success: '#2A9206',
+        error: '#EE5253',
+        accent: '#FBFBFB',
+        warning: '#FF9F43',
+      },
+    },
+  },
+  lang: {
+    locales: { en, pt },
+    current: 'pt',
+  },
+  components: {
+    VTextField,
+  },
+})
